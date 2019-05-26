179ac1a44b734fab0579a53623ba4db2ccfd73d7
Aidan Dunne <aidandunne@Aidans-MacBook-Air.local>
2019-05-27 00:11:36 +0100

    messing around with different features will look to build something functional next

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,56 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public actionSheetCtrl: ActionSheetController,public alertController: AlertController,) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Account Information',
+      buttons: [{
+        text: 'Delete Account',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Change Name',
+        icon: 'share',
+        handler: () => {
+          console.log('Name Change clicked');
+        }
+      }, {
+        text: 'Change Address',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Change Address clicked');
+        }
+      }, {
+        text: 'Edit Favorites',
+        icon: 'heart',
+        handler: () => {
+          console.log('Edit Favorites clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    });
+
+    await alert.present();
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+
+  async presentToast() {
+   const toast = await this.toastController.create({
+     buttons: [
+       {
+         side: 'start',
+         icon: 'star',
+         text: 'Favorite',
+         handler: () => {
+           console.log('Favorite clicked');
+         }
+       }, {
+         text: 'Done',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       }
+     ]
+   });
+   toast.present();
+ }
 
 }
